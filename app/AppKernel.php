9729445e577101e6a9a15c85677a60707477c3e9
kristijansoldo <soldokristijan@yahoo.com>
2017-09-30 20:03:26 +0200

    appKernel date fix

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,6 +5,18 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+	/**
+	 * AppKernel constructor.
+	 *
+	 * @param string $environment
+	 * @param bool   $debug
+	 */
+	public function __construct($environment, $debug)
+	{
+		date_default_timezone_set( 'Europe/Paris' );
+		parent::__construct($environment, $debug);
+	}
+	
     public function registerBundles()
     {
         $bundles = [
